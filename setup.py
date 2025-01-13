64ed95bc1b72b7660bdb34070d094be4a5b9debc
wuhplaptop <wuhplaptop@gmail.com>
2025-01-13 17:39:44 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="face-rec",  # The name on PyPI
+    version="0.2.0",
+    description="Future-proof Facial Recognition with YOLO + Facenet, modular detectors/embedders, hooks, CLI, etc.",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "numpy",
+        "torch",
+        "Pillow",
+        "scikit-learn",
+        "ultralytics",
+        "facenet-pytorch",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            # This means the user can type `face-rec` after installing
+            # and it will invoke the `main` function in `myfacerec.cli`
+            "face-rec=myfacerec.cli:main", 
+        ],
+    },
+)
