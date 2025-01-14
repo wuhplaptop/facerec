9a8e54c53c61db4c4546c8c73df61e9c011dcc68
wuhplaptop <wuhplaptop@gmail.com>
2025-01-14 00:40:30 -0500

    Update sample_plugin.py

--- myfacerec/plugins/sample_plugin.py
+++ myfacerec/plugins/sample_plugin.py
@@ -1,13 +1,14 @@
 # myfacerec/plugins/sample_plugin.py
 
 from ..detectors import FaceDetector
+from PIL import Image
 
 class SampleDetector(FaceDetector):
     def __init__(self):
         # Initialize your custom detector here
         pass
 
-    def detect_faces(self, image):
+    def detect_faces(self, image: Image.Image):
         # Implement custom detection logic
         # For demonstration, return an empty list
         return []
